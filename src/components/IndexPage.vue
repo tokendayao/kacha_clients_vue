568c9c35d58b83582115ef5ba08567936d1e3029
大瑶 <37734020@qq.com>
2021-01-04 10:08:32 +0000

    scroll page get data

--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -1,212 +1,299 @@
 <template>
   <div id="IndexPage">
-
-
-
-      <div id="header_div" class="container" style="margin-top: 5px" >
-        <div class="col-xl-12 text-left  "  style="padding-left:0;" >
+    <div id="header_div" class="container" style="margin-top: 5px">
+      <div class="col-xl-12 text-left  " style="padding-left:0;">
         <span class="badge badge-warning">Kacha</span>
       </div>
     </div>
-
     <!--  搜索框 BANNER START   -->
-
-    <div id="search_div" class="container h-100"  style="margin-top: 10px">
-
-
-      <div  class="row">
+    <div id="search_div" class="container h-100" style="margin-top: 10px">
+      <div class="row">
         <div class="col-xl-12">
           <div class="input-group mb-3">
             <div class="input-group-prepend">
-
-              <span class="input-group-text img-fluid"><img src="../assets/icons/search.svg" alt="" title="Bootstrap" /></span>
+              <span class="input-group-text img-fluid"><img src="../assets/icons/search.svg" alt="" title="Bootstrap" @click.prevent="searchGroups($event)" /></span>
             </div>
-            <input type="search" class="form-control" placeholder="搜索圈儿"  />
+            <input type="search" class="form-control" placeholder="搜索圈儿" v-model="keyword" @keyup.enter.prevent="searchGroups($event)" />
           </div>
         </div>
       </div>
-
-
-
-
-
-      <div id="search_banner" class="row h-100" >
+      <div id="search_banner" class="row h-100 " v-if="bannerList">
         <div class="col-xl-12">
-          <div id="carouselExampleSlidesOnly"  class="carousel slide" data-ride="carousel">
+          <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner ">
               <div v-for="(value,key)  in bannerList" :class=" [bannerActive==key?'active':'','carousel-item']">
-                <img :src="['../static/'+value.image_src]" class="  w-100 h-100 img-responsive center-block" style="position: relative" alt="..." >
-
+                <img :src="['../static/'+value.image_src]" class="  w-100 img-responsive center-block" alt="..." />
               </div>
-
             </div>
           </div>
         </div>
       </div>
-
     </div>
     <!--  搜索框 BANNER END   -->
-
-<!--  圈子列表 START  -->
-    <div class="container" :style="{'position':'relative','overflow':'auto','margin-top':'10px','height':curHeight+'px'}">
-
-
-
-        <div v-if="(n%2)!=0" class="row"v-for="n in  groupCount " style="position:relative;flex-wrap: nowrap;margin-top: 15px; " data-spy="scroll">
-
-          <div class="col-xl-6" v-if="(n%2)!=0 && k>=n " v-for="k in n+1" :style="style_lavender[Math.floor(Math.random()*6 + 1)]" >
-
-            <div  class="card text-white bg-info mb-3" style="max-width: 18rem;">
-
-              <div class="card-header">
-
-                {{ groupList[k-1].group_name }}
-              </div>
-
-              <div class="card-body">
-                <h5 class="card-title"> {{ groupList[k-1].group_slogan }}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-
+    <!--  圈子列表 START  && (k<groupCount+1) @scroll="handleScroll($event)"-->
+    <div id="group_list" class="container" v-if="groupCount" :style="{'position':'relative','overflow':'auto','margin-top':'10px','height':curHeight+'px'}">
+      <div v-if="(n%2)!=0" class="row" v-for="n in  groupCount " style="position:relative;flex-wrap: nowrap;margin-top: 15px; " data-spy="scroll">
+        <div class="col-xl-6  text-center" v-if="(n%2)!=0 &amp;&amp; (k&gt;=n)  " v-for="k in n+1" :style="style_lavender[Math.floor(Math.random()*6 + 1)]">
+          <div v-if="k&lt;(groupCount+1)" class="m-auto card text-white bg-info mb-3" style="max-width: 18rem;">
+            <div class="card-header">
+              {{ groupList[k-1].group_name }}
             </div>
-
+            <div class="card-body">
+              <h5 class="card-title"> {{ groupList[k-1].group_slogan }}</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
           </div>
-
+          <div v-else="k&gt;=(groupCount+1)" class="" style="max-width: 18rem;">
+          </div>
+        </div>
       </div>
-
-
     </div>
-
     <!--  圈子列表 END  -->
-
-<!--  底部   start-->
-    <div  class="container" style="border-top: 1px gray solid ; background-color: white; z-index:1;position:fixed;bottom:0;margin-bottom: 0;">
+    <!--  底部   start-->
+    <div class="container" style="border-top: 1px gray solid ; background-color: white; z-index:1;position:fixed;bottom:0;margin-bottom: 0;">
       <div id="bottom_div" class="row w-100 " style=" background-color: white; z-index:1;position:fixed;bottom:0;margin-bottom: 0;">
-      <nav class="w-100  " style="">
-        <ul  id="group_list_ul" class="w-100 list-group list-group-horizontal-sm text-center" style="flex-direction: row;margin-left: 10px;padding-top: 0">
-          <li class="list-group-item " style="width:33%" ><a class="nav-link" href="#">圈儿</a></li>
-          <li class="list-group-item  " style="width:33%" ><a class="nav-link" href="#">信儿 <span class="badge badge-light">4</span></a></li>
-          <li class="list-group-item  " style="width:33%" ><a class="nav-link " href="#">我的</a></li>
-        </ul>
-      </nav>
-       </div>
+        <nav class="w-100  " style="">
+          <ul id="group_list_ul" class="w-100 list-group list-group-horizontal-sm text-center" style="flex-direction: row;margin-left: 10px;padding-top: 0">
+            <li class="list-group-item " style="width:33%"><a class="nav-link" href="#">圈儿</a></li>
+            <li class="list-group-item  " style="width:33%"><a class="nav-link" href="#">信儿 <span class="badge badge-light">4</span></a></li>
+            <li class="list-group-item  " style="width:33%"><a class="nav-link " href="#">我的</a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
     <!--  底部  end -->
-
   </div>
-
 </template>
-
 <script>
   import $ from 'jquery'
-    import axios from "axios";
+  import axios from "axios";
 
-    export default {
-        name: "IndexPage",
-      data(){
-        return {
-          bannerList: [],
-          bannerActive:0,
-          groupList:[],
-          groupCount:0,
-          randStyleSeed:0,
-          curHeight:0,
-          style_lavender:[
-            {"background-color":"lavender"},
-            {"background-color":"lavender"},
-            {"background-color":"lavenderblush"},
-            {"background-color":"#FFE4B5"},
-            {"background-color":"#FFC1C1"},
-            {"background-color":"#F0FFF0"},
-            {"background-color":"#D1EEEE"},
-            {"background-color":"#C1CDC1"},
-            {"background-color":"#8968CD"}
-          ],
+  export default {
+    name: "IndexPage",
+    data(){
+      return {
+        lastTime:null,
+        pageFinish:0,
+        directFlag:0,
+        keyword:'',
+        bannerList: [],
+        bannerActive:0,
+        groupList:[],
+        groupCount:0,
+        groupTotal:0,
+        randStyleSeed:0,
+        curHeight:0,
+        scrollPage:1,
+        style_lavender:[
+          {"background-color":"lavender"},
+          {"background-color":"lavender"},
+          {"background-color":"lavenderblush"},
+          {"background-color":"#FFE4B5"},
+          {"background-color":"#FFC1C1"},
+          {"background-color":"#F0FFF0"},
+          {"background-color":"#D1EEEE"},
+          {"background-color":"#C1CDC1"},
+          {"background-color":"#8968CD"}
+        ],
+
+      }
+    },
+    computed: {
+
+    },
+    beforeCreate() {
+
+    },
+    created() {
+
+    },
+    beforeMount() {
+
+    },
+    mounted () {
+      //取BANNER
+      axios
+        .get('api/banners?type=index')
+        .then((response) => {
+          if (response.data.code == "200") {
+            // this.bannerList.push(response.data.data.list),
+            this.bannerList = response.data.data.list
+          }
+
+        })
+        .catch(function (error) { // 请求失败处理
+          console.log(error);
+        });
+
+      this.indexGroups();
+      // window.addEventListener("scroll", this.handleScroll,true);
+      window.addEventListener("scroll",this.throttle(this.handleScroll,3000),true)
+    },
+    beforeDestroy() {
+      window.removeEventListener('scroll', this.handleScroll,true)
+    },
+    beforeUpdate(){
+
+
+
+    },
+    updated(){
+      var h = document.documentElement.clientHeight || document.body.clientHeight;
+
+      this.curHeight =h -$('#header_div').height()-$('#search_div').height()-$('#bottom_div').height()-30; //减去页面上固定高度height
+    
+    },
+    methods: {
+      throttle(func, wait) {
+
+        let timeout
+        return () => {
+          let context = this;
+          let now = new Date();
+          let arg = arguments;
+
+
+          if (now - this.lastTime - wait > 0) {
+
+            if (timeout) {
+              clearTimeout(timeout)
+              timeout = null
+            }
+
+            func.apply(context, arg)
+            this.lastTime = now
+          } else if (!timeout) {
+            timeout = setTimeout(() => {
+              func.apply(context, arg)
+            }, wait)
+          }
+        }
+      },
+
+      getDirection() {
+        // 页面滚动距顶部距离
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop ||
+          document.body.scrollTop || $('#group_list').scrollTop()
+        var scroll = scrollTop - this.directFlag;
+        this.directFlag = scrollTop;
+
+        if(scroll<0){
+          return 'up'
+        }else{
+          return 'down'
 
         }
       },
-      computed: {
 
-      },
-     beforeCreate() {
+      indexGroups(){
 
-     },
-      created() {
-
-      },
-      beforeMount() {
-
-      },
-      mounted () {
-       //取BANNER
+        //取圈子列表,todo带TOKEN
         axios
-          .get('api/banners?type=index')
-          .then(response => (
+          .get('api/myGroups?page='+this.scrollPage)
+          .then((response) => {
+            if (response.data.code == "200") {
 
-              // this.bannerList.push(response.data.data.list),
-            this.bannerList=response.data.data.list
-      ))
-          .catch(function (error) { // 请求失败处理
-            console.log(error);
-          });
 
-        //取圈子列表
-        axios
-          .get('api/myGroups')
-          .then(response => (
-            this.groupList=response.data.data.list,
-              this.groupCount=response.data.data.pagination.total
+              this.groupCount =  response.data.data.list.length+this.groupList.length;
 
-          ))
+
+              this.groupTotal=response.data.data.pagination.total
+
+              for(var i=0;i<response.data.data.list.length;i++){
+
+                this.groupList.push(response.data.data.list[i]);
+
+              }
+
+
+
+            }else if(response.data.code == "3000"){
+              this.pageFinish=1;
+              this.scrollPage--;
+              console.log('没有更多数据了');
+            }
+
+
+          })
           .catch(function (error) {
             console.log(error);
           });
 
 
-
-
       },
-  beforeUpdate(){
+      searchGroups(event){
+
+        axios
+          .get('api/groups?keyword='+this.keyword+'&page='+this.scrollPage)
+          .then((response) => {
+
+            if (response.data.code == "200") {
+              this.groupList = response.data.data.list,
+                this.groupCount = response.data.data.pagination.total
+            }
 
 
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      },
 
-  },
-  updated(){
+      handleScroll(event){
+        //窗口滚要做的操作写这里
+        let sh = document.documentElement.scrollHeight || document.body.scrollHeight; // 滚动条高度
+        let st = document.documentElement.scrollTop || document.body.scrollTop; // 滚动条距离顶部的距离
+        let ch = document.documentElement.clientHeight || document.body.clientHeight; // 滚动条外容器的高度
 
-    var h = document.documentElement.clientHeight || document.body.clientHeight;
+        if (st + ch >= sh-20) {
 
-    this.curHeight =h -$('#header_div').height()-$('#search_div').height()-$('#bottom_div').height(); //减去页面上固定高度height
+          //快到底时----加载
+          if(this.scrollPage <this.groupTotal){
 
-    console.log($('#search_div').height());
 
-  },
-      // methods: {
-      //   getRandomInt(){
-      //     // alert(Math.ceil(Math.random()*6)),
-      //    this.data.randStyleSeed=Math.ceil(Math.random()*6)
-      //     }
-      // }
+            if(this.getDirection()=='down'){
+
+
+              if(this.pageFinish!=1){
+                if(this.keyword==''){
+
+                  this.scrollPage+=1;
+
+
+                  this.indexGroups();
+                }else{
+                  this.searchGroups(event);
+
+                }
+              }
+
+            }
+
+          }
+        }
+
+      }
     }
+  }
 </script>
-
-<style scoped >
+<style scoped="">
 
 
   ul{
     width: 100%;
-                margin:0;
-                padding:0;
+    margin:0;
+    padding:0;
     list-style:none;
-align-content: center;
+    align-content: center;
     overflow:hidden;
   }
-ul li{
+  ul li{
 
-                 margin:0;
-                padding:0;
-          border:none;
+    margin:0;
+    padding:0;
+    border:none;
 
 
-             }
+  }
 
 </style>
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,65 +1,50 @@
-
 <template>
   <!-- 创建要控制的区域 -->
-  <div id="app" class="container-md">
-
-
-    <div class="row" >
-
-      <div class="col-xs-6 col-sm-3 col-md-6 col-lg-9"
-           style="background-color: #dedef8;
-box-shadow: inset 1px -1px 1px #444,
-inset -1px 1px 1px #444;overflow: hidden !important; ">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat.
-        </p>
-
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-          quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo.
-        </p>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-          quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo.
-        </p>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-          quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo.
-        </p>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-          quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo.
-        </p>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-          quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo.
-        </p>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-          quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo.
-        </p>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-          quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo.
-        </p>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-          quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo.
-        </p>
+  <div id="app">
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        <h3 class="panel-title">Add Product</h3>
       </div>
+      <div class="panel-body form-inline">
+        <label>
+          Id:
+          <input type="text" class="form-control" v-model="id">
+        </label>
 
+        <label>
+          Name:
+          <input type="text" class="form-control" v-model="name" @keyup.enter="add">
+        </label>
 
+        <label>Keywords Search:
+          <!-- 注意 ：Vue中所有指令，在调用的时候，都以v- 开头-->
+          <input type="text" class="form-control" v-model="keywords">
+        </label>
+
+        <input type="button" value="Add" class="btn btn-primary" @click="add">
+      </div>
     </div>
+
+    <table class="table table-hover table-bordered table-striped">
+      <thead>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Time</th>
+        <th>Operation</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in list" >
+        <td>{{item.id}}</td>
+        <td>{{item.name }}</td>
+        <td>{{item.ctime}}</td>
+        <td>
+          <a href="#" @click.prevent="del(item.id)">Delete</a>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -77,7 +62,71 @@ inset -1px 1px 1px #444;overflow: hidden !important; ">
         name: "",
         keywords: ""
       };
+    },
+    methods: {
+      add() {
+        // vue中已经实现了数据的双向绑定，每当我们修改了data中的数据，Vue会默认监听到
+        // 数据的改动，自动把最新的数据，应用到页面上
+        this.list.push({ id: this.id, name: this.name, ctime: new Date() });
+      },
+
+      del(id) {
+        // 根据Id删除数据
+        // this.list.some((item, i) => {
+        //     if (item.id == id) {
+        //         this.list.splice(i, 1)
+        //         // 在数组的 some 方法中，如果return true ，就会立即终止这个数组的后续循环
+        //         return true
+        //     }
+        // })
+
+        let index = this.list.findIndex(item => {
+          if (item.id == id) {
+            return true;
+          }
+        });
+
+        this.list.splice(index, 1);
+      },
+
+      search(keywords) {
+        var newList = [];
+        // this.list.forEach(item => {
+        //     if (item.name.indexOf(keywords) != -1) {
+        //         newList.push(item)
+        //     }
+        // });
+        // return newList;
+
+        // forEach some fliter findIndex 这些都属于数组的新方法，
+        // 都会对数组的每一项，进行遍历，执行相关的操作
+        return this.list.filter(item => {
+          //注意:在ES6中，为字符串提供了一个新方法，叫做 String.prototype.includes("要包含的字符串")
+          // 如果包含，返回true，反之false
+          if (item.name.includes(keywords)) {
+            return item;
+          }
+        });
+      }
     }
   };
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
